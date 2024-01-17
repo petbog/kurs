dfc8bc91cf1beb9a6b3f17ffcdce7b5c444c5f0a
petrov bogdan <bogdan_emdetei_petrov@mail.ru>
2024-01-17 13:23:17 +0300

    Babel. Extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -7,7 +7,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
 
 
 
-  
+
     const svgLoader = {
         test: /\.svg$/i,
         issuer: /\.[jt]sx?$/,
@@ -15,7 +15,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     }
 
 
-    
+
 
     const cssLoader = {
         test: /\.s[ac]ss$/i,
@@ -50,12 +50,31 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
             },
         ],
     }
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                "plugins": [
+                    ["i18next-extract",
+                        {
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true
+                        }
+                    ]
+                ]
+            }
+        }
+    }
 
 
     return [
+        babelLoader,
         typescriptLoader,
         cssLoader,
         svgLoader,
-        fileLoader
+        fileLoader,
     ]
 }

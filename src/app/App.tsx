ce2ec620b46ecc9438c8794a7bb03d85465e38f2
Петров Богдан <bogdan_emdetei_petrov@mail.ru>
2024-03-05 15:19:10 +0300

    modal keydown

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,7 +4,9 @@ import { classNames } from '../helpers/classNames/classNames'
 import { AppRouter } from './provider/router'
 import { NavBar } from './widgets/NavBar'
 import { Sidebar } from './widgets/Sidebar'
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
+import Modal from './shered/ui/Modal/Modal'
+
 
 
 export enum Theme {
@@ -17,12 +19,16 @@ export enum Theme {
 const App = () => {
     const { theme } = useTheme()
 
-
+    const [isOpen, setIsOpen] = useState(false)
 
     return (
         <div className={classNames('App', {}, [theme])}>
             <Suspense fallback='' >
                 <NavBar />
+                <button onClick={() => { setIsOpen(true) }} >open</button>
+                <Modal isOpen={isOpen} onClose={() => { setIsOpen(false) }} >
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis, dicta doloremque quae odit reprehenderit quod consectetur ipsam laborum ea eligendi rem modi harum accusantium totam omnis perspiciatis necessitatibus minima corrupti.
+                </Modal>
                 <div className="content-page">
                     <Sidebar />
                     <AppRouter />
---
+++ src/app/shered/ui/Modal/Modal.tsx
@@ -0,0 +1,72 @@
+import { classNames } from "helpers/classNames/classNames"
+import cls from './Modal.module.scss'
+import { ReactNode, useCallback, useEffect, useRef, useState } from "react"
+
+
+
+interface ModalProps {
+  className?: string,
+  children?: ReactNode,
+  isOpen?: boolean,
+  onClose?: () => void
+}
+
+const Modal = (props: ModalProps) => {
+  const { className, children, isOpen, onClose } = props
+  const [isClosing, setIsClosing] = useState(false)
+  const timerRef = useRef<ReturnType<typeof setTimeout>>()
+
+  const ANIMATION_DELAU = 500
+
+  const closeHandler = () => {
+    if (onClose) {
+      onClose()
+      setIsClosing(true)
+      timerRef.current = setTimeout(() => {
+        onClose()
+        setIsClosing(false)
+      }, ANIMATION_DELAU)
+    }
+  }
+
+  const onContentClick = (e: React.MouseEvent) => {
+    e.stopPropagation()
+  }
+
+  const onKeyDown =useCallback( (e: KeyboardEvent) => {
+    if (e.key === 'Escape') {
+      closeHandler()
+    }
+  },[closeHandler])
+
+  useEffect(() => {
+    if (isOpen) {
+      window.addEventListener('keydown', onKeyDown)
+    }
+
+
+    return () => {
+      clearTimeout(timerRef.current)
+      window.removeEventListener('keydown', onKeyDown)
+    }
+
+  }, [isOpen,onKeyDown])
+
+  const mods: Record<string, boolean> = {
+    [cls.isOpened]: isOpen,
+    [cls.isClosin]: isClosing
+  }
+
+  return (
+    <div className={classNames(cls.Modal, mods, [className])}>
+      <div onClick={closeHandler} className={cls.overlay}>
+        <div onClick={onContentClick} className={cls.content}>
+          {children}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+export default Modal
